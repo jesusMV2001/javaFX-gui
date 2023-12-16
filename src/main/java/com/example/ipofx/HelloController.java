fc29c2927bb08acc86d45a2fad1814e41ad3ca25
jesus <jmv00037@red.ujaen.es>
2023-12-16 20:23:45 +0100

    creando menus y idiomas

--- src/main/java/com/example/ipofx/HelloController.java
+++ src/main/java/com/example/ipofx/HelloController.java
@@ -1,14 +1,25 @@
 package com.example.ipofx;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
 
 public class HelloController {
+    public CheckMenuItem idioma2;
+    public CheckMenuItem idioma1;
     @FXML
     private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected void onIdioma1() {
+        //TODO hacer que cuando se seleccione cambie al idioma 1
+        if(idioma1.isSelected()) idioma2.setSelected(false);
+        if(!idioma1.isSelected() && !idioma2.isSelected()) idioma1.setSelected(true);
+    }
+
+    @FXML
+    protected void onIdioma2() {
+        //TODO hacer que cuando se seleccione cambie al idioma 2
+        if(idioma2.isSelected()) idioma1.setSelected(false);
+        if(!idioma1.isSelected() && !idioma2.isSelected()) idioma2.setSelected(true);
     }
 }
